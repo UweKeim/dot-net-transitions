33de46d6d1e9f8b81be54b1c37769210d62ff7c1
richard_s_shepherd@yahoo.co.uk <richard_s_shepherd@yahoo.co.uk@493a881e-6b3f-11de-bf08-17995f091a54>
2009-07-09 18:34:52 +0000

    Critical damping transition method.

---
+++ trunk/Transitions/TransitionMethod_CriticalDamping.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transitions
+{
+	public class TransitionMethod_CriticalDamping : ITransitionMethod
+	{
+		#region Public methods
+
+		/// <summary>
+		/// Constructor. You pass in the time that the transition 
+		/// will take (in milliseconds).
+		/// </summary>
+		public TransitionMethod_CriticalDamping(int iTransitionTime)
+		{
+			if (iTransitionTime <= 0)
+			{
+				throw new Exception("Transition time must be greater than zero.");
+			}
+			m_dTransitionTime = iTransitionTime;
+		}
+
+		#endregion
+
+		#region ITransitionMethod Members
+
+		/// <summary>
+		/// </summary>
+		public void onTimer(int iTime, out double dPercentage, out bool bCompleted)
+		{
+			// We find the percentage time elapsed...
+			double dElapsed = iTime / m_dTransitionTime;
+			dPercentage = (1.0 - Math.Exp(-1.0 * dElapsed * 5)) / 0.993262053;
+
+			if (dElapsed >= 1.0)
+			{
+				dPercentage = 1.0;
+				bCompleted = true;
+			}
+			else
+			{
+				bCompleted = false;
+			}
+		}
+
+		#endregion
+
+		#region Private data
+
+		private double m_dTransitionTime = 0.0;
+
+		#endregion
+	}
+}
