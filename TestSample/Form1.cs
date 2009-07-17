5a4b815cd177960ca6eafdc1d6d9ed4337f78fe2
richard_s_shepherd <richard_s_shepherd@yahoo.co.uk>
2009-07-17 21:00:23 +0000

    Working on the sample.

--- TestSample/Form1.cs
+++ TestSample/Form1.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 
 namespace TestSample
 {
+    /// <summary>
+    /// This form demonstrates a number of animated transitions using the Transitions
+    /// library. Each event handler (form-load, button click) demonstrates a different
+    /// transition.
+    /// </summary>
     public partial class Form1 : Form
     {
         #region Public methods
@@ -22,82 +27,29 @@ namespace TestSample
 
         #region Form event handlers
 
+        /// <summary>
+        /// Called when the form loads.
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
-            Transition.run(this, "Opacity", 0.0, 1.0, new TransitionType_Deceleration(800));
-        }
-        
-        private void cmdCrossFadePictures_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void cmdBounceMe_Click(object sender, EventArgs e)
-        {
-            int iDestination = gbBounce.Height - cmdBounceMe.Height;
-            Transition.run(cmdBounceMe, "Top", iDestination, new TransitionType_Bounce(1500));
+            // We animate the Opacity property of the form, to fade-in the form.
+            // The Deceleration transition animates starting at a high rate of change
+            // and slowing down to zero by the end of the transition...
+            Transition.run(this, "Opacity", 1.0, new TransitionType_Deceleration(800));
         }
 
-        private void cmdFlashMe_Click(object sender, EventArgs e)
-        {
-            Transition.run(cmdFlashMe, "BackColor", Color.Pink, new TransitionType_Flash(2, 300));
-        }
-
-        private void cmdRipple_Click(object sender, EventArgs e)
-        {
-            ctrlRipple.ripple();
-        }
-
-        private void cmdDropAndBounce_Click(object sender, EventArgs e)
-        {
-            IList<TransitionElement> elements = new List<TransitionElement>();
-            elements.Add(new TransitionElement(40, 100, InterpolationMethod.Accleration));
-            elements.Add(new TransitionElement(65, 65, InterpolationMethod.Deceleration));
-            elements.Add(new TransitionElement(80, 100, InterpolationMethod.Accleration));
-            elements.Add(new TransitionElement(90, 90, InterpolationMethod.Deceleration));
-            elements.Add(new TransitionElement(100, 100, InterpolationMethod.Accleration));
-   
-            int iDestination = gbDropAndBounce.Height - cmdDropAndBounce.Height - 10;
-            Transition.run(cmdDropAndBounce, "Top", iDestination, new TransitionType_UserDefined(elements, 2000));
-
-            Transition t1 = new Transition(new TransitionType_Linear(2000));
-            t1.add(cmdDropAndBounce, "Left", cmdDropAndBounce.Left + 400);
-
-            Transition t2 = new Transition(new TransitionType_EaseInEaseOut(2000));
-            t2.add(cmdDropAndBounce, "Top", 19);
-            t2.add(cmdDropAndBounce, "Left", 6);
-
-            Transition.runChain(t1, t2);
-        }
-
-        private void cmdThrowAndCatch_Click(object sender, EventArgs e)
-        {
-            Transition.run(cmdThrowAndCatch, "Top", 12, new TransitionType_ThrowAndCatch(1500));
-        }
-
-        private void cmdTextTransition_Click(object sender, EventArgs e)
-        {
-            Transition t = new Transition(new TransitionType_Linear(1000));
-            if (lblTextTransition1.Text == "Hello, World!")
-            {
-                t.add(lblTextTransition1, "Text", "A longer piece of text.");
-                t.add(lblTextTransition1, "ForeColor", Color.Red);
-
-                t.add(lblTextTransition2, "Text", "Hello, World!");
-                t.add(lblTextTransition2, "ForeColor", Color.Blue);
-            }
-            else
-            {
-                t.add(lblTextTransition1, "Text", "Hello, World!");
-                t.add(lblTextTransition1, "ForeColor", Color.Blue);
-
-                t.add(lblTextTransition2, "Text", "A longer piece of text.");
-                t.add(lblTextTransition2, "ForeColor", Color.Red);
-            }
-            t.run();
-        }
-
+        /// <summary>
+        /// Called when the "Swap" button is pressed.
+        /// </summary>
         private void cmdSwap_Click(object sender, EventArgs e)
         {
+            // We swap over the group-boxes that show the "Bounce" and 
+            // "Throw and Catch" transitions. The active one is animated 
+            // left off the screen and the inactive one is animated right
+            // onto the screen...
+
+            // We work out which box is currently on screen and
+            // which is off screen...
             Control ctrlOnScreen, ctrlOffScreen;
             if (gbBounce.Left == GROUP_BOX_LEFT)
             {
@@ -112,16 +64,160 @@ namespace TestSample
             ctrlOnScreen.SendToBack();
             ctrlOffScreen.BringToFront();
 
+            // We create a transition to animate the two boxes simultaneously. One is
+            // animated onto the screen, the other off the screen.
+
+            // The ease-in-ease-out transition acclerates the rate of change for the 
+            // first half of the animation, and decelerates during the second half.
+
             Transition t = new Transition(new TransitionType_EaseInEaseOut(1000));
             t.add(ctrlOnScreen, "Left", -1 * ctrlOnScreen.Width);
             t.add(ctrlOffScreen, "Left", GROUP_BOX_LEFT);
             t.run();
         }
 
+        /// <summary>
+        /// Called when the "Bounce Me!" button is pressed.
+        /// </summary>
+        private void cmdBounceMe_Click(object sender, EventArgs e)
+        {
+            // We bounce the button down to the bottom of the group box it is in, and 
+            // back up again.
+
+            // The Bounce transition accelerates the property to its destination value
+            // (as if with gravity) and decelerates it back  to its original value (as
+            // if against gravity).
+
+            int iDestination = gbBounce.Height - cmdBounceMe.Height;
+            Transition.run(cmdBounceMe, "Top", iDestination, new TransitionType_Bounce(1500));
+        }
+
+        /// <summary>
+        /// Called when the "Throw and Catch" button is pressed.
+        /// </summary>
+        private void cmdThrowAndCatch_Click(object sender, EventArgs e)
+        {
+            // The button is 'thrown' up to the top of the group-box it is in
+            // and then falls back down again. 
+
+            // The throw-and-catch transition starts the animation at a high rate and
+            // decelerates to zero (as if against gravity) at the destination value. It 
+            // then accelerates the value (as if with gravity) back to the original value.
+
+            Transition.run(cmdThrowAndCatch, "Top", 12, new TransitionType_ThrowAndCatch(1500));
+        }
+
+        /// <summary>
+        /// Called when the "Flash Me!" button is pressed.
+        /// </summary>
+        private void cmdFlashMe_Click(object sender, EventArgs e)
+        {
+            // The Flash transition animates the property to the destination value
+            // and back again. You specify how many flashes to show and the length
+            // of each flash...
+            Transition.run(cmdFlashMe, "BackColor", Color.Pink, new TransitionType_Flash(2, 300));
+        }
+
+        /// <summary>
+        /// Called when the "Ripple" button is pressed.
+        /// </summary>
+        private void cmdRipple_Click(object sender, EventArgs e)
+        {
+            // The ripple is handled by the RippleControl user-control.
+            // This performs 100 simultaneous animations to create the 
+            // ripple effect...
+            ctrlRipple.ripple();
+        }
+
+        /// <summary>
+        /// Called when the "Text Transition" button is pressed.
+        /// </summary>
+        private void cmdTextTransition_Click(object sender, EventArgs e)
+        {
+            // We transition four properties simulataneously here:
+            // - The two labels' text is changed.
+            // - The two labels' colors are changed.
+
+            // We work out the new text and colors to transition to...
+            string strText1, strText2;
+            Color color1, color2;
+            if (lblTextTransition1.Text == STRING_SHORT)
+            {
+                strText1 = STRING_LONG;
+                color1 = Color.Red;
+                strText2 = STRING_SHORT;
+                color2 = Color.Blue;
+            }
+            else
+            {
+                strText1 = STRING_SHORT;
+                color1 = Color.Blue;
+                strText2 = STRING_LONG;
+                color2 = Color.Red;
+            }
+
+            // We create a transition to animate all four properties at the same time...
+            Transition t = new Transition(new TransitionType_Linear(1000));
+            t.add(lblTextTransition1, "Text", strText1);
+            t.add(lblTextTransition1, "ForeColor", color1);
+            t.add(lblTextTransition2, "Text", strText2);
+            t.add(lblTextTransition2, "ForeColor", color2);
+            t.run();
+        }
+
+        /// <summary>
+        /// Called when the "Change Form Color" button is pressed.
+        /// </summary>
         private void ctrlChangeFormColor_Click(object sender, EventArgs e)
         {
+            // We alternate the form's background color...
             Color destination = (BackColor == BACKCOLOR_PINK) ? BACK_COLOR_YELLOW : BACKCOLOR_PINK;
             Transition.run(this, "BackColor", destination, new TransitionType_Linear(1000));
+        }
+
+        /// <summary>
+        /// Called when the "More" or "Less" button is pressed.
+        /// </summary>
+        private void cmdMore_Click(object sender, EventArgs e)
+        {
+            // We either show more screen or less screen depending on the current state.
+            // We find out whether we need to make the screen wider or narrower...
+            int iFormWidth;
+            if (cmdMore.Text == "More >>")
+            {
+                iFormWidth = 984;
+                cmdMore.Text = "<< Less";
+            }
+            else
+            {
+                iFormWidth = 452;
+                cmdMore.Text = "More >>";
+            }
+
+            // We animate it with an ease-in-ease-out transition...
+            Transition.run(this, "Width", iFormWidth, new TransitionType_EaseInEaseOut(1000));
+        }
+
+        private void cmdDropAndBounce_Click(object sender, EventArgs e)
+        {
+            IList<TransitionElement> elements = new List<TransitionElement>();
+            elements.Add(new TransitionElement(40, 100, InterpolationMethod.Accleration));
+            elements.Add(new TransitionElement(65, 65, InterpolationMethod.Deceleration));
+            elements.Add(new TransitionElement(80, 100, InterpolationMethod.Accleration));
+            elements.Add(new TransitionElement(90, 90, InterpolationMethod.Deceleration));
+            elements.Add(new TransitionElement(100, 100, InterpolationMethod.Accleration));
+
+            int iDestination = gbDropAndBounce.Height - cmdDropAndBounce.Height - 10;
+            Transition.run(cmdDropAndBounce, "Top", iDestination, new TransitionType_UserDefined(elements, 2000));
+
+            Transition t1 = new Transition(new TransitionType_Linear(2000));
+            t1.add(cmdDropAndBounce, "Left", cmdDropAndBounce.Left + 400);
+
+            Transition t2 = new Transition(new TransitionType_EaseInEaseOut(2000));
+            t2.add(cmdDropAndBounce, "Top", 19);
+            t2.add(cmdDropAndBounce, "Left", 6);
+
+            Transition.runChain(t1, t2);
         }
 
         #endregion
@@ -135,23 +231,11 @@ namespace TestSample
         // The left point of the 'bounce' and 'throw-and-catch' group boxes...
         private const int GROUP_BOX_LEFT = 12;
 
+        // Strings used for the text transition...
+        private const string STRING_SHORT = "Hello, World!";
+        private const string STRING_LONG = "A longer piece of text.";
+
         #endregion
 
-        private void cmdMore_Click(object sender, EventArgs e)
-        {
-            int iFormWidth;
-            if (cmdMore.Text == "More >>")
-            {
-                iFormWidth = 984;
-                cmdMore.Text = "<< Less";
-            }
-            else
-            {
-                iFormWidth = 452;
-                cmdMore.Text = "More >>";
-            }
-
-            Transition.run(this, "Width", iFormWidth, new TransitionType_EaseInEaseOut(1000));
-        }
     }
 }
