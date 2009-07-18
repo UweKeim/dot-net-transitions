760363cd186290a311a4ba9fb940f4a5e109b8b7
richard_s_shepherd <richard_s_shepherd@yahoo.co.uk>
2009-07-18 19:26:01 +0000

    Working on the sample.

--- TestSample/Form1.Designer.cs
+++ TestSample/Form1.Designer.cs
@@ -42,16 +42,20 @@
             this.cmdSwap = new System.Windows.Forms.Button();
             this.ctrlChangeFormColor = new System.Windows.Forms.Button();
             this.gbRipple = new System.Windows.Forms.GroupBox();
+            this.ctrlRipple = new TestSample.RippleControl();
             this.gbFlash = new System.Windows.Forms.GroupBox();
             this.gbDropAndBounce = new System.Windows.Forms.GroupBox();
             this.cmdMore = new System.Windows.Forms.Button();
-            this.ctrlRipple = new TestSample.RippleControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmdSwapPictures = new System.Windows.Forms.Button();
+            this.ctrlPictures = new TestSample.KittenPuppyControl();
             this.groupBox1.SuspendLayout();
             this.gbBounce.SuspendLayout();
             this.gbThrowAndCatch.SuspendLayout();
             this.gbRipple.SuspendLayout();
             this.gbFlash.SuspendLayout();
             this.gbDropAndBounce.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmdBounceMe
@@ -113,9 +117,9 @@
             this.groupBox1.Controls.Add(this.lblTextTransition2);
             this.groupBox1.Controls.Add(this.lblTextTransition1);
             this.groupBox1.Controls.Add(this.cmdTextTransition);
-            this.groupBox1.Location = new System.Drawing.Point(229, 348);
+            this.groupBox1.Location = new System.Drawing.Point(229, 397);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(211, 144);
+            this.groupBox1.Size = new System.Drawing.Size(211, 101);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Text transition";
@@ -125,7 +129,7 @@
             this.lblTextTransition2.AutoSize = true;
             this.lblTextTransition2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTextTransition2.ForeColor = System.Drawing.Color.Crimson;
-            this.lblTextTransition2.Location = new System.Drawing.Point(13, 93);
+            this.lblTextTransition2.Location = new System.Drawing.Point(7, 75);
             this.lblTextTransition2.Name = "lblTextTransition2";
             this.lblTextTransition2.Size = new System.Drawing.Size(192, 16);
             this.lblTextTransition2.TabIndex = 15;
@@ -136,7 +140,7 @@
             this.lblTextTransition1.AutoSize = true;
             this.lblTextTransition1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTextTransition1.ForeColor = System.Drawing.Color.Blue;
-            this.lblTextTransition1.Location = new System.Drawing.Point(13, 68);
+            this.lblTextTransition1.Location = new System.Drawing.Point(6, 59);
             this.lblTextTransition1.Name = "lblTextTransition1";
             this.lblTextTransition1.Size = new System.Drawing.Size(112, 16);
             this.lblTextTransition1.TabIndex = 14;
@@ -202,17 +206,24 @@
             this.gbRipple.Controls.Add(this.ctrlRipple);
             this.gbRipple.Location = new System.Drawing.Point(229, 50);
             this.gbRipple.Name = "gbRipple";
-            this.gbRipple.Size = new System.Drawing.Size(211, 292);
+            this.gbRipple.Size = new System.Drawing.Size(211, 211);
             this.gbRipple.TabIndex = 18;
             this.gbRipple.TabStop = false;
             this.gbRipple.Text = "Ripple";
             // 
+            // ctrlRipple
+            // 
+            this.ctrlRipple.Location = new System.Drawing.Point(6, 62);
+            this.ctrlRipple.Name = "ctrlRipple";
+            this.ctrlRipple.Size = new System.Drawing.Size(193, 140);
+            this.ctrlRipple.TabIndex = 8;
+            // 
             // gbFlash
             // 
             this.gbFlash.Controls.Add(this.cmdFlashMe);
-            this.gbFlash.Location = new System.Drawing.Point(229, 498);
+            this.gbFlash.Location = new System.Drawing.Point(229, 504);
             this.gbFlash.Name = "gbFlash";
-            this.gbFlash.Size = new System.Drawing.Size(211, 67);
+            this.gbFlash.Size = new System.Drawing.Size(211, 61);
             this.gbFlash.TabIndex = 19;
             this.gbFlash.TabStop = false;
             this.gbFlash.Text = "Flash the Button";
@@ -238,12 +249,33 @@
             this.cmdMore.UseVisualStyleBackColor = true;
             this.cmdMore.Click += new System.EventHandler(this.cmdMore_Click);
             // 
-            // ctrlRipple
+            // groupBox2
             // 
-            this.ctrlRipple.Location = new System.Drawing.Point(6, 62);
-            this.ctrlRipple.Name = "ctrlRipple";
-            this.ctrlRipple.Size = new System.Drawing.Size(193, 224);
-            this.ctrlRipple.TabIndex = 8;
+            this.groupBox2.Controls.Add(this.cmdSwapPictures);
+            this.groupBox2.Controls.Add(this.ctrlPictures);
+            this.groupBox2.Location = new System.Drawing.Point(229, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(211, 124);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Picture transition";
+            // 
+            // cmdSwapPictures
+            // 
+            this.cmdSwapPictures.Location = new System.Drawing.Point(10, 19);
+            this.cmdSwapPictures.Name = "cmdSwapPictures";
+            this.cmdSwapPictures.Size = new System.Drawing.Size(73, 90);
+            this.cmdSwapPictures.TabIndex = 1;
+            this.cmdSwapPictures.Text = "Swap Pictures";
+            this.cmdSwapPictures.UseVisualStyleBackColor = true;
+            this.cmdSwapPictures.Click += new System.EventHandler(this.cmdSwapPictures_Click);
+            // 
+            // ctrlPictures
+            // 
+            this.ctrlPictures.Location = new System.Drawing.Point(95, 19);
+            this.ctrlPictures.Name = "ctrlPictures";
+            this.ctrlPictures.Size = new System.Drawing.Size(103, 90);
+            this.ctrlPictures.TabIndex = 0;
             // 
             // Form1
             // 
@@ -251,6 +283,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(220)))));
             this.ClientSize = new System.Drawing.Size(445, 571);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.cmdMore);
             this.Controls.Add(this.gbDropAndBounce);
             this.Controls.Add(this.gbFlash);
@@ -261,9 +294,7 @@
             this.Controls.Add(this.gbBounce);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Opacity = 0;
             this.Text = "Transitions sample";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.gbBounce.ResumeLayout(false);
@@ -271,6 +302,7 @@
             this.gbRipple.ResumeLayout(false);
             this.gbFlash.ResumeLayout(false);
             this.gbDropAndBounce.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -295,6 +327,9 @@
         private System.Windows.Forms.GroupBox gbFlash;
         private System.Windows.Forms.GroupBox gbDropAndBounce;
         private System.Windows.Forms.Button cmdMore;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button cmdSwapPictures;
+        private KittenPuppyControl ctrlPictures;
     }
 }
 
